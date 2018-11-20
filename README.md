4b7e463a6fb40629d619a29ee47f97157098cfcb
solomon <odumahs@gmail.com>
2018-11-20 16:42:59 +0100

    setup react router

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+[![Build Status](https://travis-ci.org/slimsolz/MyDiary-React.svg?branch=develop)](https://travis-ci.org/slimsolz/MyDiary-React)
+
 # MyDiary-React
--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,17 @@
 import React from 'react'
-import './App.css';
+import { Switch, Route } from 'react-router-dom';
+
+import Header from './Header/Header';
+import Main from './Main/Main';
+import './App.scss';
 
 export default function App() {
   return (
     <div>
+      <Header />
+      <Switch>
+        <Route exact path='/login' component={Main} />
+      </Switch>
       <h1>My Diary app</h1>
     </div>
   )
